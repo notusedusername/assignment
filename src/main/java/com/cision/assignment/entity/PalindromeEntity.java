dd90280c99b1c835c972933a6452653200c8366c
Norbert Toth <norbert.toth@nebu.com>
2022-10-05 21:34:29 +0200

    Added Flyway migration

--- src/main/java/com/cision/assignment/entity/PalindromeEntity.java
+++ src/main/java/com/cision/assignment/entity/PalindromeEntity.java
@@ -5,15 +5,13 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.time.ZonedDateTime;
 
 @Data
 @Entity
+@Table(name = "palindrome")
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
